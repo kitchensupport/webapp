acc5779c56d3f41d9770a7ee6668e63e7a448562
Garrick <gbuckle@purdue.edu>
2015-10-14 20:29:30 -0400

    Display some real recipes.

--- src/js/controllers/RecipeController.js
+++ src/js/controllers/RecipeController.js
@@ -1,3 +1,37 @@
-function RecipeController() {}
+function RecipeController($scope, $http) {
+    $scope.featuredRecipes = {};
+    $scope.searchedRecipes = {};
 
-export default [RecipeController];
+    $scope.getFeaturedRecipes = () => {
+        try {
+            $http.get('http://localhost:8000/recipe/featured')
+                .success((response) => {
+                    $scope.featuredRecipes = response.data.matches;
+                    return 'SUCCESS';
+                })
+                .error(() => {
+                    return 'FAILURE';
+                });
+        } catch (err) {
+            return 'ERROR';
+        }
+    };
+a
+    $scope.getSearchRecipes = (searchTerm) => {
+        try {
+            $http.get(`http://localhost:8000/recipe/search/${searchTerm}`)
+                .success((response) => {
+                    $scope.searchedRecipes[searchTerm] = response.data.matches;
+                    console.log(response.data.matches);
+                    return 'SUCCESS';
+                })
+                .error(() => {
+                    return 'FAILURE';
+                });
+        } catch (err) {
+            return 'ERROR';
+        }
+    };
+}
+
+export default ['$scope', '$http', RecipeController];
