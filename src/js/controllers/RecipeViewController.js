f4205994af2474bba55238fde42aa58858cd8cf7
Garrick <gbuckle@purdue.edu>
2015-10-16 18:52:46 -0400

    Favorited recipes UI elements.

--- src/js/controllers/RecipeViewController.js
+++ src/js/controllers/RecipeViewController.js
@@ -26,12 +26,46 @@ function RecipeViewController($scope, $http, $state, $stateParams) {
 
                     return true;
                 } else {
-                    console.log(`FAILb: ${JSON.stringify(response)}`);
+                    console.log(`FAIL: ${JSON.stringify(response)}`);
                     return false;
                 }
             }, (err) => {
-                console.log(`FAILa: ${JSON.stringify(err)}`);
+                console.log(`FAIL: ${JSON.stringify(err)}`);
                 $scope.recipe.status = 500;
+                return false;
+            });
+    };
+
+    $scope.favoriteRecipe = () => {
+        $scope.recipe.data.favorited = true;
+
+        $http.post(`http://api.kitchen.support/recipes/favorite`, {id: $scope.recipe.data.id})
+            .then((response) => {
+                if (response && response.status === 200) {
+                    return true;
+                } else {
+                    console.log(`FAIL: ${JSON.stringify(response)}`);
+                    return false;
+                }
+            }, (err) => {
+                console.log(`FAIL: ${JSON.stringify(err)}`);
+                return false;
+            });
+    };
+
+    $scope.unFavoriteRecipe = () => {
+        $scope.recipe.data.favorited = false;
+
+        $http.post(`http://api.kitchen.support/recipes/unfavorite`, {id: $scope.recipe.data.id})
+            .then((response) => {
+                if (response && response.status === 200) {
+                    return true;
+                } else {
+                    console.log(`FAIL: ${JSON.stringify(response)}`);
+                    return false;
+                }
+            }, (err) => {
+                console.log(`FAIL: ${JSON.stringify(err)}`);
                 return false;
             });
     };
