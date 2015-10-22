3bfa5a283aaeecad9e54a374b954a03f9024624d
Garrick <gbuckle@purdue.edu>
2015-10-22 16:01:06 -0400

    Does this really have to be lowercase?

--- tests/conf.js
+++ tests/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
-    chromeDriver: '../node_modules/protractor/selenium/chromeDriver',
+    chromeDriver: '../node_modules/protractor/selenium/chromedriver',
     framework: 'jasmine2',
     specs: ['main.js'],
     sauceUser: process.env.SAUCE_USERNAME,
