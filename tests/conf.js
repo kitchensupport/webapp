cf96445caea7d84e51f474985fe66a2427faac83
Garrick <gbuckle@purdue.edu>
2015-10-22 20:39:45 -0400

    Remove unnecessary environmental variables.

--- tests/conf.js
+++ tests/conf.js
@@ -2,7 +2,5 @@ exports.config = {
     seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
     chromeDriver: '../node_modules/protractor/selenium/chromedriver',
     framework: 'jasmine2',
-    specs: ['main.js'],
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY
+    specs: ['main.js']
 };
