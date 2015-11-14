9793e49145cc12d2adb293fd362b827ad18551bc
Garrick <gbuckle@purdue.edu>
2015-11-14 17:07:45 -0500

    Fix http calls.

--- src/js/services/IngredientService.js
+++ src/js/services/IngredientService.js
@@ -23,22 +23,20 @@ function parseIngredients(promise) {
 function RecipeService($http, $q, AuthService) {
     return {
         getIngredients: (params = {}) => {
-
             const {limit = 30, offset = 0} = params;
 
             return parseIngredients($http.get(`http://api.kitchen.support/ingredients?limit=${limit}&offset=${offset}`));
         },
         getPantry: (params = {}) => {
-            console.log(`token: ${AuthService.getApiToken()}`);
             const {limit = 30, offset = 0} = params;
 
             return parseIngredients($http.get(`http://api.kitchen.support/pantry?api_token=${AuthService.getApiToken()}&limit=${limit}&offset=${offset}`));
         },
         addPantry: (ingredientId) => {
-            return parseIngredients($http.post(`http://api.kitchen.support/pantry?api_token=${AuthService.getApiToken()}&ingredient_id=${ingredientId}`));
+            return $http.post(`http://api.kitchen.support/pantry`, {api_token: AuthService.getApiToken(), ingredient_id: ingredientId});
         },
         removePantry: (ingredientId) => {
-            return parseIngredients($http.delete(`http://api.kitchen.support/pantry?api_token=${AuthService.getApiToken()}&ingredient_id=${ingredientId}`));
+            return $http.delete(`http://api.kitchen.support/pantry`, {api_token: AuthService.getApiToken(), ingredient_id: ingredientId});
         }
     };
 }
